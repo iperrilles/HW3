879c5bd607cdd48093fabe4bf9c71624c0d8ea94
iperrilles <isaac-perrilles@uiowa.edu>
2019-10-11 03:09:12 -0500

    Working calculator & css

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def index():
     return render_template('index.html', display="", pageTitle='Loan Payment Calculator')
 
 @app.route('/calculate', methods=['GET', 'POST'])
-def addition():
+def calculation():
     if request.method == 'POST':
         form = request.form
-        A = int(form['A'])
-        n = int(form['nperiod']) * int(form['nyears'])
-        i = int(form['i']) / int(form['nperiod'])
-        D =  ((( 1 + i ) ^n ) - 1 ) / ( i ( 1+ i) ^n)
+        A = float(form['Amount'])
+        n = float(form['nperiod']) * float(form['nyears'])
+        i = float(form['interest']) / float(form['nperiod'])
+        D = ((( 1 + i)**n) - 1 ) / ( i * ( 1+ i)**n)
         P = A/D
         return render_template('index.html', display=P, Discount=D, pageTitle='Loan Payment Calculator')
 
